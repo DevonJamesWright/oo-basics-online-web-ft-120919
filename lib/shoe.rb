b30df21a0be0a3f3e1afc8d00de7bb11a86ba4e1
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-17 18:36:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,5 @@
 class Shoe
-  attr_accessor :shoe_brand, :color, :size, :material, :condition
+  attr_accessor :brand, :color, :size, :material, :condition
   def intialize (brand = "Adidas")
     @brand = "#{brand}"
   end
